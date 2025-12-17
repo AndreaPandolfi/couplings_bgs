9599264a386fea342682f657af99503815bfb93c
Andrea Pandolfi <3182891@phd.unibocconi.it>
2025-12-17 15:27:03 +0100

    added file_string function

--- coupling_bgs/utils.py
+++ coupling_bgs/utils.py
@@ -7,3 +7,9 @@ def compute_bound(d0, K, kappa, eps, rho):
     eps = np.asarray(eps)
     rho = np.asarray(rho)
     return 1 + (np.log(d0) + np.log(K) + 2.5 * np.log(kappa) - 2 * np.log(eps)) / -np.log(rho)
+
+
+def file_string(reg_num: int, K: int, collapsed: bool) -> str:
+    coll_str = "collapsed" if collapsed else "vanilla"
+    
+    return f"reg{reg_num}_K{K}_{coll_str}"
